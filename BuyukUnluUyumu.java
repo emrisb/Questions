7545e3894f5a95282f58dc047e7fe7e9be00b772
Emre <emre.isbarali@marun.edu.tr>
2017-11-24 11:12:46 +0300

    some changes are applied

--- BuyukUnluUyumu.java
+++ BuyukUnluUyumu.java
@@ -3,53 +3,47 @@ import java.util.Scanner;
 public class BuyukUnluUyumu {
 
 	public static void main(String[] args) {
-
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a word: ");
 		String word = scan.nextLine();
 		scan.close();
 		BuyukUnluUyumu b = new BuyukUnluUyumu();
-		b.new ControlState(word).controlWhat();
-
+		b.new ControlState().controlWhat(word);
+		
 	}
 
 	class ControlState {
-		char[] kalin = { 'a', 'ı', 'u', 'o', 'A', 'I', 'U', 'O' };
-		char[] ince = { 'e', 'i', 'ü', 'ö', 'E', 'İ', 'Ü', 'Ö' };
-		String mWord = "";
+		final char[] thickVowels = { 'a', 'ı', 'u', 'o', 'A', 'I', 'U', 'O' };
+		final char[] thinVowels = { 'e', 'i', 'ü', 'ö', 'E', 'İ', 'Ü', 'Ö' };
 
-		ControlState(String word) {
-			this.mWord = word;
-		}
-
-		void controlWhat() {
-			for (int i = 0; i < getSize(); i++) {
-				for (int j = 0; j < kalin.length; j++) {
-					if (mWord.charAt(i) == kalin[j]) {
-						printResult(ince, i);
+		public void controlWhat(String word) {
+			for (int i = 0; i < getSize(word); i++) {
+				for (int j = 0; j < thickVowels.length; j++) {
+					if (word.charAt(i) == thickVowels[j]) {
+						printResult(thinVowels, i, word);
 						return;
-					} else if (mWord.charAt(i) == ince[j]) {
-						printResult(kalin, i);
+					} else if (word.charAt(i) == thinVowels[j]) {
+						printResult(thickVowels, i, word);
 						return;
 					}
 				}
 			}
 		}
 
-		void printResult(char[] arr, int n) {
-			for (int k = n + 1; k < getSize(); k++) {
+		void printResult(char[] arr, int n, String word) {
+			for (int k = n + 1; k < getSize(word); k++) {
 				for (int l = 0; l < arr.length; l++) {
-					if (mWord.charAt(k) == arr[l]) {
-						System.out.println(mWord + " kelimesi " + "büyük ünlü uyumuna uymaz.");
+					if (word.charAt(k) == arr[l]) {
+						System.out.println(word + " kelimesi " + "büyük ünlü uyumuna uymaz.");
 						return;
 					}
 				}
 			}
-			System.out.println(mWord + " kelimesi " + "büyük ünlü uyumuna uyar.");
+			System.out.println(word + " kelimesi " + "büyük ünlü uyumuna uyar.");
 		}
 
-		int getSize() {
-			return mWord.length();
+		int getSize(String word) {
+			return word.length();
 		}
 	}
 }
